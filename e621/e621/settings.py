a17a8eff7c2b6609eeb56237b943065ca824a94a
Madison Scott-Clary <madison@archive.org>
2018-12-11 20:34:41 +0000

    Update dev settings, requirements

--- e621/e621/settings.py
+++ e621/e621/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
+    'django_extensions',
     'posts',
     'reports',
 ]
@@ -120,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
